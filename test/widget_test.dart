459b800e2ee753e7d7832b626d51fb6824f64559
amaralkaff <tpamangly@gmail.com>
2024-11-08 11:28:18 +0800

    fix: landscape camera

---
+++ test/widget_test.dart
@@ -0,0 +1,15 @@
+// test/widget_test.dart
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:WorkoutAI/main.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // You can write your tests here
+    expect(find.byType(MaterialApp), findsOneWidget);
+  });
+}
